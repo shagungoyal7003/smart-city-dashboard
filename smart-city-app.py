10c37498a2c81b8e2a9c98b72adfd2f7eef40d69
Shagun Goyal <shagungoyal7003@gmail.com>
2026-03-24 12:57:36 +0530

    Delete smart-city-app.py

--- smart-city-app.py
+++
@@ -1,7 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-from .models import WeatherData, AirQuality, TrafficData
-
-admin.site.register(WeatherData)
-admin.site.register(AirQuality)
-admin.site.register(TrafficData)
